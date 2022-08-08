a1a71a6af679fe31d1ee6d3951d5469d3c9bf00a
Podavalenko <deidra2@yandex.ru>
2022-08-08 21:39:42 +0300

    data class Post updated and some classes added

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -1,13 +1,61 @@
 data class Post (
     val postId: Long,
     val ownerId: Long = 0,
+    val fromId: Int,
+    val createdBy: Int,
     val date: Int = 0,
-    val text: String = "",
+    val text: String = "any text",
+    val replyOwnerId: Int,
+    val replyPostId: Int,
     val friendsOnly: Boolean = false,
+    val comments: Comments? = null,
+    val copyright: Copyright? = null,
+    val likes: Likes? = null,
+    val reposts: Reposts? = null,
+    val views: Views? = null,
     val postType: String = "any type",
+    val postSource: PostSource? = null,
+    val geo: Geo? = null,
+    val signerId: Int,
     val canPin: Boolean = false,
     val canDelete: Boolean = false,
     val canEdit: Boolean = false,
+    val isPinned: Boolean = false,
+    val markeAsAds: Boolean = false,
     val isFavorite: Boolean = false,
-    val likes: Likes ?= null,
+    val donut: Donut? = null,
+    val postponedId: Int,
+    val attachments: Array<Attachment> = emptyArray()
 )
+
+interface Attachment {
+    val type: String
+}
+class PhotoAttachment : Attachment {
+    override val type: String = "photo"
+    val photo: Photo = Photo()
+}
+
+class Photo (
+    val id: Int? = null,
+    val ownerId: Int? = null,
+    val photo130: String? = null,
+    val photo604: String? = null,
+    val postedPhoto: Boolean = false
+)
+
+class VideoAttachment : Attachment {
+    override val type: String = "video"
+    val video: Video = Video()
+
+}
+
+class Video (
+    val id: Int? = null,
+    val duration: Int? = null,
+    val title: String? = null,
+    val views: Int? = null,
+    val player: String? = null,
+)
+
+
---
+++ src/main/kotlin/PostSource.kt
@@ -0,0 +1,6 @@
+class PostSource (
+      val type: String, //Тип источника
+      val platform: String, //Название платформы, если оно доступно
+      val data: String, //Тип действия
+      val url: String, //URL ресурса, с которого была опубликована запись.
+        )
